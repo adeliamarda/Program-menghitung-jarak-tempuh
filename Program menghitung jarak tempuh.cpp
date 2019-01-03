09fd8d9789c2bc6d46d21dfdcedbef6cd6ea1107
Adelia Marda Trisno <46318729+adeliamarda@users.noreply.github.com>
2019-01-03 19:21:20 +0700

    Add files via upload

---
+++ Program menghitung jarak tempuh.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float v,s,t;
+    cout<<"program menghitung jarak tempuh"<<endl;
+    cout<<"masukan nilai s"<<endl;
+    cin>>s;
+    cout<<"masukan nilai t"<<endl;
+    cin>>t;
+    cout<<"maka nilai v adalah:"<<v<<endl;
+    return 0;
+}
